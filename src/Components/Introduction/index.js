cce4a32afcf81158c8a06d7e161cd6ba60e5642a
Sebastian Infante Murguia <107079587+sebas-inf@users.noreply.github.com>
2023-01-11 10:26:42 -0500

    Update index.js

--- src/Components/Introduction/index.js
+++ src/Components/Introduction/index.js
@@ -10,7 +10,7 @@ const Introduction = () => {
                 <div class="intro">
                     <img class="self_image" src={ProfilePic}/>
                     <div class="intro__section">
-                        <h1 class = "intro__title">Hi, my name is Sebastian Infante Murguia</h1>
+                        <h1 class = "intro__title">Hi, my name is Sebastian Infante</h1>
                         <p class = "intro__subtitle">Computer Engineering Undergraduate</p>
                         <p class = "intro__memo">I am currently a student in the University of South Florida in the Engineering department seeking an internship in Software Engineering.</p>
                         <a href= "mailto: sebinfante12@gmail.com" class="header-button">Contact Me</a>
